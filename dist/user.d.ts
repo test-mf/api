aa96349f411fbca72898ed2078a0e1e7db3df9c2
d0kur0 <dev.dokuro@gmail.com>
2022-06-03 19:54:06 +0300

    chore: remove dist from repo, add to ignore

--- dist/user.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function isUserLogin(): boolean;
-declare type UserNumber = {
-    buyDate: Date;
-    number: string;
-    status: "active" | "block";
-    type: "ABC" | "FREE" | "DEF";
-};
-export declare function getUserNumbers(): Promise<UserNumber[]>;
-export {};
